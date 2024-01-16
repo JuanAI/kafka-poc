bd3ce30eb66854b01f88b935127366b69888d46e
Juan Hernández <39837599+JuanAI@users.noreply.github.com>
2024-01-16 06:51:04 +0000

    python consumer connected to kafka

--- consumer/python/consumer1.py
+++ consumer/python/consumer1.py
@@ -1,4 +1,4 @@
-from confluent_kafka import Consumer, KafkaException
+from kafka import KafkaConsumer
 import redis
 import os
 from dotenv import load_dotenv
@@ -9,7 +9,7 @@ load_dotenv()
 # Kafka configuration
 kafkaHost = os.environ.get("KF_HOST", "localhost")
 kafkaPort = os.environ.get("KF_PORT", "9092")
-kafkaTopic = os.environ.get("KF_TOPIC", "stock-market")
+kafkaTopic = os.environ.get("KF_TOPIC", "stock-market1")
 
 # Redis configuration
 redis_host = "redis-server"
@@ -19,52 +19,55 @@ redis_port = 6379
 r = redis.Redis(host=redis_host, port=redis_port, db=0)
 
 # Kafka consumer configuration
-conf = {
-    "bootstrap.servers": f"{kafkaHost}:{kafkaPort}",
-    "group.id": "stock-group1",
-    "auto.offset.reset": "earliest",
-}
-consumer = Consumer(conf)
+consumer = KafkaConsumer(
+    # Default port is 9092. If no servers are specified, will default to localhost:9092.
+    kafkaTopic,
+    group_id="stock-group1",
+    bootstrap_servers="localhost:9092",
+)
+
+for msg in consumer:
+    print(msg.value)  # CONTINUE HERE!!
 
 
-def insert_data_into_redis(topic, message):
-    r.lpush(topic, message)
-    r.ltrim(topic, 0, 99)
+# def insert_data_into_redis(topic, message):
+#     r.lpush(topic, message)
+#     r.ltrim(topic, 0, 99)
 
 
-def read_items_from_redis(topic, amount_of_data):
-    return r.lrange(topic, 0, amount_of_data - 1)
+# def read_items_from_redis(topic, amount_of_data):
+#     return r.lrange(topic, 0, amount_of_data - 1)
 
 
-def run():
-    try:
-        consumer.subscribe([kafkaTopic])
+# def run():
+#     try:
+#         consumer.subscribe([kafkaTopic])
 
-        while True:
-            msg = consumer.poll(timeout=1.0)
-            if msg is None:
-                continue
-            if msg.error():
-                raise KafkaException(msg.error())
-            else:
-                # Print the received message
-                print(f"Received message: {msg.value().decode('utf-8')}")
-                try:
-                    # Insert data into Redis
-                    insert_data_into_redis(kafkaTopic, msg.value().decode("utf-8"))
-                except Exception as e:
-                    print(f"Error inserting data into Redis: {e}")
+#         while True:
+#             msg = consumer.poll(timeout=1.0)
+#             if msg is None:
+#                 continue
+#             if msg.error():
+#                 raise KafkaException(msg.error())
+#             else:
+#                 # Print the received message
+#                 print(f"Received message: {msg.value().decode('utf-8')}")
+#                 try:
+#                     # Insert data into Redis
+#                     insert_data_into_redis(kafkaTopic, msg.value().decode("utf-8"))
+#                 except Exception as e:
+#                     print(f"Error inserting data into Redis: {e}")
 
-                try:
-                    # Read the last 10 items from Redis
-                    prices = read_items_from_redis(kafkaTopic, 10)
-                    print(f"Last 10 items from Redis DB: {prices}")
-                except Exception as e:
-                    print(f"Error fetching items from Redis: {e}")
+#                 try:
+#                     # Read the last 10 items from Redis
+#                     prices = read_items_from_redis(kafkaTopic, 10)
+#                     print(f"Last 10 items from Redis DB: {prices}")
+#                 except Exception as e:
+#                     print(f"Error fetching items from Redis: {e}")
 
-    finally:
-        # Close down consumer to commit final offsets.
-        consumer.close()
+#     finally:
+#         # Close down consumer to commit final offsets.
+#         consumer.close()
 
 
 if __name__ == "__main__":
